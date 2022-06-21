e603b304dd926f05767cb4287589e3733eb35bba
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-21 11:05:07 -0400

    finish with all features

--- pages/updateValidation.php
+++ pages/updateValidation.php
@@ -33,47 +33,42 @@ switch ($who) {
       $sql = 'UPDATE teachers SET tchName =? WHERE teacher_id =?';
       $adminup= $bdd->prepare($sql);
       $adminup->execute(array($_POST['name'], $id));
-      header('Location: adminprofil.php?error=true');
-      die();
+      echo "Name updated successfully \n";
     }
 
     if (isset($_POST['surname']) and $_POST['surname'] !== $surname) {
       $sql = 'UPDATE teachers SET tchSurname =? WHERE teacher_id =?';
       $adminup= $bdd->prepare($sql);
       $adminup->execute(array($_POST['surname'], $id));
-      header('Location: adminprofil.php?error=true');
-      die();
+      echo "Surname updated successfully \n";
     }
 
     if (isset($_POST['school']) and $_POST['school'] !== $school) {
       $sql = 'UPDATE teachers SET schoolName =? WHERE teacher_id =?';
       $adminup= $bdd->prepare($sql);
       $adminup->execute(array($_POST['school'], $id));
-      print_r($adminup);
+      echo "School name updated successfully \n";
     }
 
     if (isset($_POST['contact']) and $_POST['contact'] !== $contact) {
       $sql = 'UPDATE teachers SET contact =? WHERE teacher_id =?';
       $adminup= $bdd->prepare($sql);
       $adminup->execute(array($_POST['contact'], $id));
-      header('Location: adminprofil.php?error=true');
-      die();
+      echo "Contact updated successfully \n";
     }
 
     if (isset($_POST['qualification']) and $_POST['qualification'] !== $qualification) {
       $sql = 'UPDATE teachers SET qualification =? WHERE teacher_id =?';
       $adminup= $bdd->prepare($sql);
       $adminup->execute(array($_POST['qualification'], $id));
-      header('Location: adminprofil.php?error=true');
-      die();
+      echo "Qualification updated successfully \n";
     }
 
     if (isset($_POST['email']) and $_POST['email'] !== $email) {
       $sql = 'UPDATE teachers SET tchEmail =? WHERE teacher_id = ?';
       $adminup= $bdd->prepare($sql);
       $adminup->execute(array($_POST['email'], $id));
-      header('Location: ../deconnexion.php');
-      die();
+      echo "Email updated successfully \n Please login again";
     }
 
     if (isset($_POST['password']) && !empty($_POST['password']) and isset($_POST['2password'])) {
